b2724e231fb8f924cc822b5fbc88b9fd3f5e7c21
charleschia <sadkatty@163.com>
2018-12-05 17:39:48 +0800

    Add files via upload

--- main.php
+++ main.php
@@ -3,7 +3,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	</head>
 <body>
-	<form action="data_process.php" method="post">
+	<form action="main.php" method="post">
 		<tr>
 			<td>班组</td>
 			<td><select name="class">
@@ -28,7 +28,45 @@
 			<td colspan="2" align="center"><input type="submit" value="查询" /></td>
 		</tr>
 	</form>
+	<?php
 	
+	/*echo '<h4>班组岗位星级评价表</h4></br>'
+	
+	//$dbhost = 'localhost:3306';  // mysql服务器主机地址
+	//$dbuser = 'root';            // mysql用户名
+	//$dbpass = 'jiazijian';          // mysql用户名密码
+	//$conn = mysqli_connect($dbhost, $dbuser, $dbpass,'basic_info',3306);
+	$conn = mysqli_connect('localhost','root','jiazijian','basic_info',3306);
+if(! $conn )
+{
+    die('连接失败: ' . mysqli_error($conn));
+}
+echo '连接成功<br />';
+
+mysqli_select_db($conn, 'emis' );
+mysqli_close($conn);*/
+@ $db = new mysqli('localhost','root','jiazijian','eims');
+if (mysqli_connect_errno()) {
+     echo 'Error: Could not connect to database.  Please try again later.';
+     exit;
+  }
+$query = "select * from basic_info";
+  $result = $db->query($query);
+  $num_results = $result->num_rows;
+
+  echo "<p>Number of workers found: ".$num_results."</p>";
+  for ($i=0; $i <$num_results; $i++) {
+     $row = $result->fetch_assoc();
+     echo "<p><strong>".($i+1).". Title: ";
+     echo htmlspecialchars(stripslashes($row['title']));
+     echo "</strong><br />Author: ";
+     echo stripslashes($row['author']);
+     echo "<br />ISBN: ";
+     echo stripslashes($row['isbn']);
+     echo "<br />Price: ";
+     echo stripslashes($row['price']);
+     echo "</p>";
+	?>
 	
 </body>
 </html>
